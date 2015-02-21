3b28f59196f7ed0ed68da5f98b1181b4a727ed0a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-02-21 18:17:52 +0100

    Universal App Conditional XAML Sample

---
+++ ConditionalXAML/ConditionalXAML/ConditionalXAML.Shared/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ConditionalXAML.Views
+{
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
